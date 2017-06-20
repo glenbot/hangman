bf752b16a3643d9b3693605b148e058b5c234598
Glen Zangirolami <glenbot@gmail.com>
2017-06-20 11:00:53 -0500

    Fixed visualizer image

--- swarm-visualizer.sh
+++ swarm-visualizer.sh
@@ -4,7 +4,6 @@ eval $(docker-machine env node1)
 docker service create \
   --name=viz \
   --publish=8888:8080/tcp \
-  --update-delay 10s \
   --constraint=node.role==manager \
   --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
-  manomarks/visualizer
+  dockersamples/visualizer
